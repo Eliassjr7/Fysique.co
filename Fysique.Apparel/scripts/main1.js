f113a31955cdbf40fda636d20ba8fc6735627d7c
eliassjt7 <felix.eliassaint7@gmail.com>
2026-09-22 00:43:54 -0400

    drop

--- Fysique.Apparel/scripts/main1.js
+++ Fysique.Apparel/scripts/main1.js
@@ -80,7 +80,7 @@ $(function(){
     }
     setupSlider('#productslider1',12);
     setupSlider('#productslider2',12);
-    setupSlider('#productslider3',12);
+    setupSlider('#productslider3',5);
 });
 
 const userCardTemplate = document.querySelector("[data-user-template]")
